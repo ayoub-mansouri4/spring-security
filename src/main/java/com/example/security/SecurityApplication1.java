82810c8d2175a142d5a4bf07b384cd6cf581a27c
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-08 03:50:21 +0100

    commit nbr2

--- src/main/java/com/example/security/SecurityApplication1.java
+++ src/main/java/com/example/security/SecurityApplication1.java
@@ -3,11 +3,13 @@ package com.example.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SecurityApplication1 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication1.class, args);
+		
 	}
 
 }
