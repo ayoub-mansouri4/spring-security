f39971f94b0700ec572f30b1a2d6c4dd69ddeaa2
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-11 02:59:04 +0100

    commit nbr1

--- src/main/java/com/example/security/config/NewAuthenticationProvider.java
+++ src/main/java/com/example/security/config/NewAuthenticationProvider.java
@@ -1,4 +1,17 @@
 package com.example.security.config;
 
-public class NewAuthenticationProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class NewAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
 }
