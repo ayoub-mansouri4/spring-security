2d14a5b4127074c92406ac187121e9be6ef4561d
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-11 03:12:33 +0100

    commit nbr19

--- src/main/java/com/example/security/config/NewAuthenticationProvider.java
+++ src/main/java/com/example/security/config/NewAuthenticationProvider.java
@@ -4,11 +4,16 @@ import com.example.security.models.Customer;
 import com.example.security.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -23,7 +28,17 @@ public class NewAuthenticationProvider implements AuthenticationProvider {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
         List<Customer> customer = customerRepository.findCustomerByEmail(username);
-        return null;
+        if (customer.size() > 0) {
+            if (passwordEncoder.matches(password, customer.get(0).getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
     }
 
     @Override
