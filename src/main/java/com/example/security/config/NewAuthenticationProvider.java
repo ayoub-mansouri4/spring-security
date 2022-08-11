0ef71bfb86dc8ccef01606299c4ca7bbae2332f4
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-11 03:01:42 +0100

    commit nbr14

--- src/main/java/com/example/security/config/NewAuthenticationProvider.java
+++ src/main/java/com/example/security/config/NewAuthenticationProvider.java
@@ -1,10 +1,18 @@
 package com.example.security.config;
 
+import com.example.security.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class NewAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private CustomerRepo customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         return null;
