14d0f203cbfabf9cf85a81031d7c5e7504760f0d
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-11 03:04:46 +0100

    commit nbr17

--- src/main/java/com/example/security/config/NewAuthenticationProvider.java
+++ src/main/java/com/example/security/config/NewAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package com.example.security.config;
 
+import com.example.security.models.Customer;
 import com.example.security.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -17,6 +18,7 @@ public class NewAuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findCustomerByEmail(username);
         return null;
     }
 
