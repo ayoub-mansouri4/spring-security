b192b6ff48e7bcb314d61d466e813f6163d5ed70
ayoub-mansouri4 <ayoubmns11@gmail.com>
2022-08-13 12:55:54 +0100

    commit nbr21

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -8,24 +8,66 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Collections;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /**
-     * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
-     * Secured /notices - Not Secured /contact - Not Secured
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and().csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().
+                authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated()
+                .antMatchers("/user").authenticated().antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll().and().httpBasic();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////@Override
+    //protected void configure(HttpSecurity http) throws Exception {
 
         /**
          * Default configurations which will secure all the requests
          */
 
 
-          http .authorizeRequests() .anyRequest().authenticated() .and()
+          /*http .authorizeRequests() .anyRequest().authenticated() .and()
           .formLogin().and() .httpBasic();
+           */
 
 
         /**
@@ -55,11 +97,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
        /* http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
                 .httpBasic();
-        */
-
-
-    }
-
+        *////////////}
 
 
     /**
